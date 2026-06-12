f7b8d3d0da9191b54eec948a3338cb58227c2c95
mobilececo <mobilececo@gmail.com>
2026-06-12 23:38:03 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,3 +7,17 @@ def send(chat_id, msg):
         f"https://api.telegram.org/bot{TOKEN}/sendMessage",
         data={"chat_id": chat_id, "text": msg}
     )
+import requests
+
+TOKEN = "7756549139:AAHiBgFspGrKGbbnir0tNPx6DtGM_baRjX0"
+
+def send(chat_id, msg):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+    r = requests.post(url, data={
+        "chat_id": chat_id,
+        "text": msg
+    })
+
+    print("STATUS:", r.status_code)
+    print("RESPONSE:", r.text)
